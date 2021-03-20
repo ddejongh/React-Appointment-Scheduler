adbc8eae20690c7c6a8b534a566c34d8209d6b2b
Devin DeJongh <devindejongh@gmail.com>
2021-03-19 18:00:14 -0700

    moved functionality to components

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -2,8 +2,14 @@ import React, { Component }  from 'react';
 
 class ListAppointments extends Component {
     render() {
+        const listItems = this.props.appointments.map(item => (
+            <div>
+                <div>{item.petName}</div>
+                <div>{item.ownerName}</div>
+            </div>
+        ));
         return(
-            <div>List--Appointments</div>
+            <div>{listItems}</div>
         );
     }
 }
